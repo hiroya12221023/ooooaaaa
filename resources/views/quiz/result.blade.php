6823bbecc6de6450b74b84ef630c4afa81115229
hiroya12221023 <a1211065@kuins.ac.jp>
2023-10-25 08:10:00 +0000

    9

--- resources/views/quiz/result.blade.php
+++ resources/views/quiz/result.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>kuis</title>
-    <link rel="stylesheet" href="/hiro/blog/public/css/hiro.css">
      <style>
         body {
             font-family: Arial, sans-serif;
